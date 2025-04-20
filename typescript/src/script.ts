1fe01be1dc974437955f51c17f5239f724c508af
Abdulwariz <bookbodmas31@gmail.com>
2025-04-20 17:42:10 +0100

    99% progress: done with design and logic. Looking for what might need improvement

--- typescript/src/script.ts
+++ typescript/src/script.ts
@@ -1,10 +1,51 @@
+//get input elements
 const dayInputBox = document.getElementById('day') as HTMLInputElement;
 const monthInputBox = document.getElementById('month') as HTMLInputElement;
 const yearInputBox = document.getElementById('year') as HTMLInputElement;
 const arrowBtn = document.querySelector('.arrow_icon');
 
-arrowBtn?.addEventListener('click', () => {
-    console.log(dayInputBox);
-    console.log(monthInputBox);
-    console.log(yearInputBox);
-}) 
+//now grab result containers
+const yearResult = document.getElementById('year-result');
+const monthResult = document.getElementById('month-result');
+const dayResult = document.getElementById('day-result');
+
+//calculate age function
+function calculateAge() {
+    const day = parseInt(dayInputBox.value);
+    const month = parseInt(monthInputBox.value) - 1;
+    const year = parseInt(yearInputBox.value);
+
+    const today = new Date();
+    const birthDate = new Date(year, month, day);
+
+    //validate input
+    if(isNaN(birthDate.getTime()) || birthDate > today) {
+        console.log('There is an error here');
+        return;
+    }
+
+    //calculate difference between today's date and user's date
+    let ageYears = today.getFullYear() - birthDate.getFullYear();
+    let ageMonths = today.getMonth() - birthDate.getMonth();
+    let ageDays = today.getDate() - birthDate.getDate();
+
+    //adjust for negative values
+    if(ageDays < 0) {
+        ageMonths--;
+        ageDays += new Date(today.getFullYear(), today.getMonth(), 0).getDate();
+    }
+    if(ageMonths < 0) {
+        ageYears--;
+        ageMonths += 12;
+    }
+
+    (yearResult as HTMLElement).textContent = ageYears.toString();
+    (monthResult as HTMLElement).textContent = ageMonths.toString();
+    (dayResult as HTMLElement).textContent = ageDays.toString();
+}
+
+//submit button
+arrowBtn?.addEventListener('click', (e) => {
+    e.preventDefault();
+    calculateAge();
+})
